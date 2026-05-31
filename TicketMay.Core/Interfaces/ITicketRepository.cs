ae4724d2390c25999958b2e977712ef087b2eb16
RNV <r.nvessah@gmail.com>
2026-05-31 23:12:48 +0200

    feat: add ITicketRepository, fixed .gitignore

--- TicketMay.Core/Interfaces/ITicketRepository.cs
+++ TicketMay.Core/Interfaces/ITicketRepository.cs
@@ -2,7 +2,7 @@ using TicketMay.Core.Entities;
 namespace TicketMay.Core.Interfaces;
 
 //Database calls are always async because they wait for database response. We use Task for this.
-//An interface containes the methods and signature a class needs to have, without the implementation
+//An interface contains the methods and signature a class needs to have, without the implementation
 public interface ITicketRepository
 {
     Task<IEnumerable<Ticket>> GetAllAsync(); //IEnumerable is a collection of <T>
